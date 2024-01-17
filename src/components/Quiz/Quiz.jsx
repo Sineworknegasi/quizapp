369fc0db3aa6b3cecc7d154055163ce6e9133355
SINEWORK NEGASI <105476205+Sineworknegasi@users.noreply.github.com>
2024-01-17 10:06:34 +0300

    Quiz App finished functionality
    the day I finished

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,26 +1,121 @@
-import React, { useState } from 'react'
-import './Quiz.css'
-import { data } from '../../assets/data';
+import React, { useRef, useState } from "react";
+import "./Quiz.css";
+import { data } from "../../assets/data";
 
 const Quiz = () => {
+  let [index, setIndex] = useState(0);
+  let [question, setQuestion] = useState(data[index]);
+  let [lock, setlock] = useState(false);
+  let [score, setScore] = useState(0);
+  let [result, setresult] = useState(false)
 
-    let [index, setIndex] = useState(0);
-    let [question, setQuestion] = useState(data[index]);
+  let Option1 = useRef(null);
+  let Option2 = useRef(null);
+  let Option3 = useRef(null);
+  let Option4 = useRef(null);
+
+  let option_array = [Option1, Option2, Option3, Option4];
+
+  const checkans = (e, ans) => {
+    if (lock == false) {
+      if (question.ans === ans) {
+        e.target.classList.add("correct");
+        setlock(true);
+        setScore((prev) => prev + 1);
+      } else {
+        e.target.classList.add("wrong");
+        setlock(true);
+        option_array[question.ans - 1].current.classList.add("correct");
+      }
+    }
+  };
+
+  const next = () => {
+    if (lock == true) {
+      if (index === data.length -1) {
+        setresult(true);
+        return 0;
+      }
+      setIndex(++index);
+      setQuestion(data[index]);
+      setlock(false);
+      option_array.map((option) => {
+        option.current.classList.remove("correct");
+        option.current.classList.remove("wrong");
+        return null;
+      });
+    }
+  };
+
+  const reset = () => {
+    setIndex(0);
+    setQuestion(data[0]);
+    setScore(0)
+    setlock(false);
+    setresult(false);
+  }
   return (
-    <div className='container'>
-        <h1>Quiz App</h1>
-        <hr/>
-        <h2>1. Which device is required for the internet connection</h2>
-        <ul>
-            <li>Modem</li>
-            <li>Router</li>
-            <li>Lan Cable</li>
-            <li>Pen Drive</li>
-        </ul>
-        <button>Next</button>
-        <div className='index'> 1 of 5 questions</div>
+    <div className="container">
+      <h1>Quiz App</h1>
+      <hr />
+      {result?<></>:<>
+      <h2>
+        {index + 1}. {question.question}
+      </h2>
+      <ul>
+        <li
+          ref={Option1}
+          onClick={(e) => {
+            checkans(e, 1);
+          }}
+        >
+          {question.option1}
+        </li>
+        <li
+          ref={Option2}
+          onClick={(e) => {
+            checkans(e, 2);
+          }}
+        >
+          {question.option2}
+        </li>
+        <li
+          ref={Option3}
+          onClick={(e) => {
+            checkans(e, 3);
+          }}
+        >
+          {question.option3}
+        </li>
+        <li
+          ref={Option4}
+          onClick={(e) => {
+            checkans(e, 4);
+          }}
+        >
+          {question.option4}
+        </li>
+      </ul>
+      <button
+        onClick={() => {
+          next();
+        }}
+      >
+        Next
+      </button>
+      <div className="index">
+        {" "}
+        {index + 1} of {data.length} questions
+      </div>
+      </>}
+      {result?<><h2>
+        You Scored {score} out of {data.length}
+      </h2>
+      <button onClick={() => reset()} >Reset</button>
+      </> : <></>}
+      
     </div>
-  )
-}
+  );
+};
 
-export default Quiz
+export default Quiz;
